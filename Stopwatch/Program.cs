c01419cc3f7adb992d45d0873a117defe8071d28
Felippe <felippe_nvieira@hotmail.com>
2022-02-25 17:52:00 -0300

    Creating menu

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,8 +1,16 @@
-﻿Start();
+﻿Start(6);
 
-void Start()
+void Menu()
 {
-    int time = 10;
+    Console.Clear();
+    Console.WriteLine("S = Seconds");
+    Console.WriteLine("M = Minutes");
+    Console.WriteLine("0 - Exit");
+    Console.WriteLine("How long do you want to count?");
+}
+
+void Start(int time)
+{
     int currentTime = 0;
     
     while (currentTime != time)
@@ -12,4 +20,10 @@ void Start()
         Console.WriteLine(currentTime);
         Thread.Sleep(1000);
     }
+
+    Console.Clear();
+    Console.WriteLine("Stopwatch finished...");
+    Thread.Sleep(2500);
 }
+
+
