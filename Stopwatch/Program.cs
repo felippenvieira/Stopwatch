93d042eaf9696677ba4e8a38b013b3aaf9dd0b4c
Felippe <felippe_nvieira@hotmail.com>
2022-02-26 08:11:49 -0300

    Menu options

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -3,10 +3,13 @@
 void Menu()
 {
     Console.Clear();
-    Console.WriteLine("S = Seconds");
-    Console.WriteLine("M = Minutes");
+    Console.WriteLine("S = Seconds => 10s = 10 seconds");
+    Console.WriteLine("M = Minutes => 1m = 1 minute");
     Console.WriteLine("0 - Exit");
     Console.WriteLine("How long do you want to count?");
+
+    string data = Console.ReadLine().ToLower();
+
 }
 
 void Start(int time)
