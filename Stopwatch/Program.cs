5cada7aba81375203bd74962bad58c96aec4b9a7
Felippe <felippe_nvieira@hotmail.com>
2022-02-26 08:29:50 -0300

    Calculating time

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -10,13 +10,22 @@ void Menu()
 
     string data = Console.ReadLine().ToLower();
     char type = char.Parse(data.Substring(data.Length - 1, 1));
-    int time = int.Parse(data.Substring(0, data.Length -1));
+    int time = int.Parse(data.Substring(0, data.Length - 1));
+    int multiplier = 1;
+
+    if (type == 'm')
+        multiplier = 60;
+
+    if (time == 0)
+        System.Environment.Exit(0);
+
+    Start(time * multiplier);
 }
 
 void Start(int time)
 {
     int currentTime = 0;
-    
+
     while (currentTime != time)
     {
         Console.Clear();
@@ -28,6 +37,7 @@ void Start(int time)
     Console.Clear();
     Console.WriteLine("Stopwatch finished...");
     Thread.Sleep(2500);
+    Menu();
 }
 
 
